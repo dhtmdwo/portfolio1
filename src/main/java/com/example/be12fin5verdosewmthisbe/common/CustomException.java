527e002e0cd2b34b499de28d983507628ecd9a9e
scy <tjcksdud0221@naver.com>
2025-04-10 17:51:48 +0900

    사용자 지정 예외 파일 추가

---
+++ src/main/java/com/example/be12fin5verdosewmthisbe/common/CustomException.java
@@ -0,0 +1,14 @@
+package com.example.be12fin5verdosewmthisbe.common;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+}
