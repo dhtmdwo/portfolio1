cb361a2694581ecad6b8d5e7ac308cd62ab6402f
chan0o0seo <tjcksdud0221@naver.com>
2025-04-12 17:53:42 +0900

    카테고리 도메인 생성

---
+++ src/main/java/com/example/be12fin5verdosewmthisbe/menu_management/category/controller/CategoryController.java
@@ -0,0 +1,8 @@
+package com.example.be12fin5verdosewmthisbe.menu_management.category.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CategoryController {
+
+}
---
+++ src/main/java/com/example/be12fin5verdosewmthisbe/menu_management/category/service/CategoryService.java
@@ -0,0 +1,8 @@
+package com.example.be12fin5verdosewmthisbe.menu_management.category.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+
+}
