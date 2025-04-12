20576b996ae1091edb394ca7e6b414fb4d0231eb
chan0o0seo <tjcksdud0221@naver.com>
2025-04-12 17:54:15 +0900

    옵션 도메인 생성

---
+++ src/main/java/com/example/be12fin5verdosewmthisbe/menu_management/option/repository/OptionRepository.java
@@ -0,0 +1,8 @@
+package com.example.be12fin5verdosewmthisbe.menu_management.option.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class OptionRepository {
+
+}
