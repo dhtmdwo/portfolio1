7e9f0fd77a093b29db46ca3dc48cf8f081db2c91
chan0o0seo <tjcksdud0221@naver.com>
2025-04-12 22:07:59 +0900

    메뉴 도메인, 메뉴 엔티티에 swagger 어노테이션 추가

--- src/main/java/com/example/be12fin5verdosewmthisbe/menu_management/menu/model/Menu.java
+++ src/main/java/com/example/be12fin5verdosewmthisbe/menu_management/menu/model/Menu.java
@@ -1,6 +1,5 @@
 package com.example.be12fin5verdosewmthisbe.menu_management.menu.model;
 
-
 import com.example.be12fin5verdosewmthisbe.menu_management.category.model.Category;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
@@ -20,12 +19,14 @@ public class Menu {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Schema(description = "메뉴 ID (자동 생성)", example = "1")
     private Long id;
 
+    @Schema(description = "메뉴 이름", example = "김치찌개")
     private String name;
 
+    @Schema(description = "메뉴 가격", example = "8000")
     private int price;
-
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id")
@@ -33,7 +34,6 @@ public class Menu {
     private Category category;
 
     @OneToMany(mappedBy = "menu", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Schema(description = "메뉴 레시피 목록")
     private List<Recipe> recipes = new ArrayList<>();
-
-
 }
