f3c5a1f9e7bdf8e477f15d710727429012753345
chan0o0seo <tjcksdud0221@naver.com>
2025-04-12 19:14:21 +0900

    ERRORCODE에 카테고리 관련 목록 수정

--- src/main/java/com/example/be12fin5verdosewmthisbe/common/ErrorCode.java
+++ src/main/java/com/example/be12fin5verdosewmthisbe/common/ErrorCode.java
@@ -7,8 +7,9 @@ public enum ErrorCode {
 
     ERROR_CODE(100,"왜 실패했는지 설명 어쩌구저쩌구"),
     INVAILD_REQUEST(5001,"카테고리 타입에 맞지 않는 잘못된 요청입니다."),
-    NO_EXIST_NAME(5002,"해당 이름을 가진 카테고리가 존재하지 않습니다."),
-    EMPTY(5003,"카테고리 목록이 비어있습니다.")
+    CATEGORY_NOT_FOUND(5002,"해당 카테고리를 찾을 수 없습니다."),
+    EMPTY(5003,"카테고리 목록이 비어있습니다."),
+    CATEGORY_ALREADY_EXISTS(5004,"이미 존재하는 카테고리입니다."),
     ;
 
 
