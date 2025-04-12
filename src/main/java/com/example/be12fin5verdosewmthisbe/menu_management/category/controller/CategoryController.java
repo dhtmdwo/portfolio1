9a49ce643a5e1a33a16bb922a32ee92d9bb61fe9
chan0o0seo <tjcksdud0221@naver.com>
2025-04-12 18:35:30 +0900

    카테고리 도메인의 controller에 카테고리 등록 기능 추가

--- src/main/java/com/example/be12fin5verdosewmthisbe/menu_management/category/controller/CategoryController.java
+++ src/main/java/com/example/be12fin5verdosewmthisbe/menu_management/category/controller/CategoryController.java
@@ -1,9 +1,27 @@
 package com.example.be12fin5verdosewmthisbe.menu_management.category.controller;
 
+import com.example.be12fin5verdosewmthisbe.common.BaseResponse;
+import com.example.be12fin5verdosewmthisbe.menu_management.category.model.Category;
+import com.example.be12fin5verdosewmthisbe.menu_management.category.model.dto.CategoryDto;
+import com.example.be12fin5verdosewmthisbe.menu_management.category.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/api/category")
+@RequiredArgsConstructor
 public class CategoryController {
-    // Your code here
+    private final CategoryService categoryService;
+
+    @PostMapping("/register")
+    public BaseResponse<String> registerCategory(CategoryDto.requestDto dto) {
+        Category category = Category.builder()
+                .name(dto.getName())
+                .build();
+        categoryService.register(category);
+        return BaseResponse.success("Category registered successfully");
+    }
+
+
 }
         
