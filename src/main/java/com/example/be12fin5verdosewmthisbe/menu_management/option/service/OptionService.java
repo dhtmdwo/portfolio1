d850ad69fa30f391482b62be2d80c1f8c36c206f
chan0o0seo <tjcksdud0221@naver.com>
2025-04-12 20:12:22 +0900

    옵션 도메인, 서비스에 OptionValue 정보들 등록하는 함수 추가

--- src/main/java/com/example/be12fin5verdosewmthisbe/menu_management/option/service/OptionService.java
+++ src/main/java/com/example/be12fin5verdosewmthisbe/menu_management/option/service/OptionService.java
@@ -1,8 +1,24 @@
 package com.example.be12fin5verdosewmthisbe.menu_management.option.service;
 
+import com.example.be12fin5verdosewmthisbe.menu_management.option.model.Option;
+import com.example.be12fin5verdosewmthisbe.menu_management.option.model.OptionValue;
+import com.example.be12fin5verdosewmthisbe.menu_management.option.repository.OptionRepository;
+import com.example.be12fin5verdosewmthisbe.menu_management.option.repository.OptionValueRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service
-public class OptionService {
+import java.util.List;
 
+@Service
+@RequiredArgsConstructor
+public class OptionService {
+    private final OptionRepository optionRepository;
+    private final OptionValueRepository optionValueRepository;
+
+    public Option register(Option option) {
+        return optionRepository.save(option);
+    }
+    public void registerOptionValues(List<OptionValue> optionValues) {
+        optionValueRepository.saveAll(optionValues);
+    }
 }
