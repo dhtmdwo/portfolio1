537af39bd81e3ac43e4a34b991b86049968d9e41
chan0o0seo <tjcksdud0221@naver.com>
2025-04-12 19:57:38 +0900

    옵션 카테고리의 entity에 옵션별재고사용량 swagger 어노테이션 추가

--- src/main/java/com/example/be12fin5verdosewmthisbe/menu_management/option/model/OptionValue.java
+++ src/main/java/com/example/be12fin5verdosewmthisbe/menu_management/option/model/OptionValue.java
@@ -6,6 +6,7 @@ import lombok.*;
 
 import java.math.BigDecimal;
 
+@Schema(description = "옵션별 재고 사용 수량 정보")
 @Entity
 @Getter
 @Setter
@@ -16,13 +17,17 @@ public class OptionValue {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Schema(description = " ID (자동 생성)", example = "1")
     private Long id;
 
+    @Schema(description = "재고 ID", example = "10")
     private Long inventoryId;
 
+    @Schema(description = "사용 수량", example = "2.0")
     private BigDecimal quantity;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "option_id")
+    @Schema(description = "해당 수량 정보가 속한 옵션 정보")
     private Option option;
 }
