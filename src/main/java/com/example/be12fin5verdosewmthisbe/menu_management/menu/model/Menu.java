0d5ca1c9f4a8f6d029acb69969b96aa4a113e80f
chan0o0seo <tjcksdud0221@naver.com>
2025-04-12 22:03:28 +0900

    메뉴 도메인, 메뉴 엔티티 추가, category와 일대다 연결

--- src/main/java/com/example/be12fin5verdosewmthisbe/menu_management/menu/model/Menu.java
+++ src/main/java/com/example/be12fin5verdosewmthisbe/menu_management/menu/model/Menu.java
@@ -1,16 +1,36 @@
 package com.example.be12fin5verdosewmthisbe.menu_management.menu.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.example.be12fin5verdosewmthisbe.menu_management.category.model.Category;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "메뉴 정보")
 public class Menu {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    private String name;
+
+    private int price;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    @Schema(description = "메뉴가 속한 카테고리 정보")
+    private Category category;
+
 
 }
