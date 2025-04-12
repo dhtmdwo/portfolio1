bbf9097f5913bbdaa68e164eb9bed90590ec9ece
chan0o0seo <tjcksdud0221@naver.com>
2025-04-12 18:17:50 +0900

    카테고리 도메인의 entity 수정

--- src/main/java/com/example/be12fin5verdosewmthisbe/menu_management/category/model/Category.java
+++ src/main/java/com/example/be12fin5verdosewmthisbe/menu_management/category/model/Category.java
@@ -1,14 +1,26 @@
 package com.example.be12fin5verdosewmthisbe.menu_management.category.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@Schema(description = "메뉴 카테고리 정보")
 public class Category {
-@Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "카테고리 ID (자동 생성)", example = "1")
     private Long id;
+
+    @Column(unique = true, nullable = false)
+    @Schema(description = "카테고리 이름", example = "메인 요리")
+    private String name;
+
+    @Schema(description = "카테고리 활성화 여부", example = "true")
+    private boolean enabled = true;
+
+
 }
-        
