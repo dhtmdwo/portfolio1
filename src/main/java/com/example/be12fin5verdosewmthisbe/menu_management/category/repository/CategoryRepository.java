cf3858518c5891f4a29ebfa38d5cb5105e3a0f26
chan0o0seo <tjcksdud0221@naver.com>
2025-04-12 19:31:54 +0900

    카테고리 도메인의 repository에 findbynamecontaining 추가

--- src/main/java/com/example/be12fin5verdosewmthisbe/menu_management/category/repository/CategoryRepository.java
+++ src/main/java/com/example/be12fin5verdosewmthisbe/menu_management/category/repository/CategoryRepository.java
@@ -4,11 +4,14 @@ import com.example.be12fin5verdosewmthisbe.menu_management.category.model.Catego
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     // Your code here
     public Optional<Category> findByName(String name);
+
+    List<Category> findByNameContaining(String keyword);
 }
         
