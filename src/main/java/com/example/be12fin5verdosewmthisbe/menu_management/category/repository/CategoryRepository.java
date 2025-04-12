86595a572c4cadb3adfb0538d80542085f24be5e
chan0o0seo <tjcksdud0221@naver.com>
2025-04-12 18:16:54 +0900

    카테고리 도메인의 repository 수정

--- src/main/java/com/example/be12fin5verdosewmthisbe/menu_management/category/repository/CategoryRepository.java
+++ src/main/java/com/example/be12fin5verdosewmthisbe/menu_management/category/repository/CategoryRepository.java
@@ -1,9 +1,11 @@
 package com.example.be12fin5verdosewmthisbe.menu_management.category.repository;
 
+import com.example.be12fin5verdosewmthisbe.menu_management.category.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class CategoryRepository {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
     // Your code here
 }
         
