19b60fe2c7cc34b08c7821fbb6a55d0be9b91d97
chan0o0seo <tjcksdud0221@naver.com>
2025-04-12 19:02:28 +0900

    ERRORCODE에 카테고리 관련 목록 추가

--- src/main/java/com/example/be12fin5verdosewmthisbe/common/ErrorCode.java
+++ src/main/java/com/example/be12fin5verdosewmthisbe/common/ErrorCode.java
@@ -5,7 +5,11 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
 
-    ERROR_CODE(100,"왜 실패했는지 설명 어쩌구저쩌구");
+    ERROR_CODE(100,"왜 실패했는지 설명 어쩌구저쩌구"),
+    INVAILD_REQUEST(5001,"카테고리 타입에 맞지 않는 잘못된 요청입니다."),
+    NO_EXIST_NAME(5002,"해당 이름을 가진 카테고리가 존재하지 않습니다."),
+    EMPTY(5003,"카테고리 목록이 비어있습니다.")
+    ;
 
 
 
