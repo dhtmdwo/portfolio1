8fc962fe1412cb207771ce103de9af925b32e467
seungjae <dhtmdwo73@naver.com>
2025-04-14 17:24:11 +0900

    setter 추가

--- src/main/java/com/example/be12fin5verdosewmthisbe/user/model/User.java
+++ src/main/java/com/example/be12fin5verdosewmthisbe/user/model/User.java
@@ -5,9 +5,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 public class User {
 @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
