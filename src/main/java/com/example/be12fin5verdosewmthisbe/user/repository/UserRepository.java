ee86255afb6b2faa29492cd1e78a001207b742e0
seungjae <dhtmdwo73@naver.com>
2025-04-14 19:57:39 +0900

    user 회원가입 DTO 생성

--- src/main/java/com/example/be12fin5verdosewmthisbe/user/repository/UserRepository.java
+++ src/main/java/com/example/be12fin5verdosewmthisbe/user/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package com.example.be12fin5verdosewmthisbe.user.repository;
 
+import com.example.be12fin5verdosewmthisbe.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
     // Your code here
+    Optional<User> findByUserId(String username);
 }
         
