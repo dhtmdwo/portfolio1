87db6ac68f2b81d8cd8f803052097e006318448f
scy <tjcksdud0221@naver.com>
2025-04-15 09:49:34 +0900

    판매 재료에 등록된 이미지의 경로를 저장하는 엔티티 생성

---
+++ src/main/java/com/example/be12fin5verdosewmthisbe/market_management/market/model/image.java
@@ -0,0 +1,24 @@
+package com.example.be12fin5verdosewmthisbe.market_management.market.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+
+    private String url;
+}
