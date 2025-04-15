91ce4c521d0b0290311e2f112ca94e29c682451a
seungjae <dhtmdwo73@naver.com>
2025-04-15 18:51:04 +0900

    이메일 조회 구현중

---
+++ src/main/java/com/example/be12fin5verdosewmthisbe/user/model/dto/UserInfoDto.java
@@ -0,0 +1,57 @@
+package com.example.be12fin5verdosewmthisbe.user.model.dto;
+
+import com.example.be12fin5verdosewmthisbe.user.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class UserInfoDto {
+
+    @Getter
+    public static class updateRequest {
+
+        private String name;
+        private String email;
+        private String password;
+        private String businessNumber;
+        private String phoneNumber;
+        private String ssn;
+        public User toEntity(String encodedPassword) {
+            User user = User.builder()
+                    .name(name)
+                    .email(email)
+                    .password(encodedPassword)
+                    .businessNumber(businessNumber)
+                    .phoneNumber(phoneNumber)
+                    .ssn(ssn)
+                    .build();
+            return user;
+        }
+    }
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    @Builder
+    public static class SearchResponse {
+        private String phoneNumber;
+        private String name;
+        private String email;
+        private String ssn;
+
+        public static UserInfoDto.SearchResponse from(User user) {
+            String newSsn = user.getSsn();
+            if(newSsn != null && newSsn.length() > 8) {
+                newSsn = newSsn.substring(0, 8) + "*".repeat(6);
+            }
+
+            return UserInfoDto.SearchResponse.builder()
+                    .phoneNumber(user.getPhoneNumber())
+                    .name(user.getName())
+                    .email(user.getEmail())
+                    .ssn(newSsn)
+                    .build();
+        }
+    }
+}
