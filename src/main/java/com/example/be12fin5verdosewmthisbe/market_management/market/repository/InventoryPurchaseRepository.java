d405235f8a84a058c477556a3fa4e7d07fde2b5d
scy <tjcksdud0221@naver.com>
2025-04-15 14:55:38 +0900

    마켓 도메인, 거래내역 조회, 이름으로 검색, 날짜순 정렬, 배송확정 기능 추가

--- src/main/java/com/example/be12fin5verdosewmthisbe/market_management/market/repository/InventoryPurchaseRepository.java
+++ src/main/java/com/example/be12fin5verdosewmthisbe/market_management/market/repository/InventoryPurchaseRepository.java
@@ -4,8 +4,10 @@ import com.example.be12fin5verdosewmthisbe.market_management.market.model.Invent
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface InventoryPurchaseRepository extends JpaRepository<InventoryPurchase, Integer> {
-    // Your code here
+public interface InventoryPurchaseRepository extends JpaRepository<InventoryPurchase, Long> {
+    public List<InventoryPurchase> findInventoryPurchaseByBuyerStoreId(Long buyerStoreId);
 }
         
