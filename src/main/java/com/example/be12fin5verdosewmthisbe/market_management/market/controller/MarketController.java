bbb72a1ab46fca5ac3ce3def6e73087e54b1347c
scy <tjcksdud0221@naver.com>
2025-04-15 10:30:02 +0900

    마켓 도메인, 판매 & 구매 등록 api 생성

--- src/main/java/com/example/be12fin5verdosewmthisbe/market_management/market/controller/MarketController.java
+++ src/main/java/com/example/be12fin5verdosewmthisbe/market_management/market/controller/MarketController.java
@@ -1,9 +1,29 @@
 package com.example.be12fin5verdosewmthisbe.market_management.market.controller;
 
+import com.example.be12fin5verdosewmthisbe.common.BaseResponse;
+import com.example.be12fin5verdosewmthisbe.market_management.market.model.dto.InventoryPurchaseDto;
+import com.example.be12fin5verdosewmthisbe.market_management.market.model.dto.InventorySaleDto;
+import com.example.be12fin5verdosewmthisbe.market_management.market.service.MarketService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/market")
+@RequiredArgsConstructor
 public class MarketController {
-    // Your code here
+    private final MarketService marketService;
+
+    @PostMapping
+    public BaseResponse<String> registerInventorySale(@RequestBody InventorySaleDto.InventorySaleRequestDto dto) {
+        marketService.saleRegister(dto);
+        return BaseResponse.success("ok");
+    }
+    @PostMapping
+    public BaseResponse<String> register(@RequestBody InventoryPurchaseDto.InventoryPurchaseRequestDto dto) {
+        marketService.purchaseRegister(dto);
+        return BaseResponse.success("ok");
+    }
 }
-        
