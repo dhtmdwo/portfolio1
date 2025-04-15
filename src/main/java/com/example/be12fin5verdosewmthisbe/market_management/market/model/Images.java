6bc9bfab42c2e216f62e5ecfa85a6d73bcce7774
scy <tjcksdud0221@naver.com>
2025-04-15 09:54:52 +0900

    InventorSale과 Images 일대다 연결

--- src/main/java/com/example/be12fin5verdosewmthisbe/market_management/market/model/Images.java
+++ src/main/java/com/example/be12fin5verdosewmthisbe/market_management/market/model/Images.java
@@ -1,9 +1,7 @@
 package com.example.be12fin5verdosewmthisbe.market_management.market.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,11 +12,16 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class image {
+public class Images {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "inventory_sale_id")
+    @Schema(description = "이미지 경로들이 속한 판매 테이블 정보")
+    private InventorySale inventorySale;
 
     private String url;
 }
--- src/main/java/com/example/be12fin5verdosewmthisbe/market_management/market/model/InventorySale.java
+++ src/main/java/com/example/be12fin5verdosewmthisbe/market_management/market/model/InventorySale.java
@@ -1,6 +1,6 @@
 package com.example.be12fin5verdosewmthisbe.market_management.market.model;
 
-import com.example.be12fin5verdosewmthisbe.menu_management.menu.model.Recipe;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -8,7 +8,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.swing.border.EmptyBorder;
+
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.util.ArrayList;
@@ -42,6 +42,10 @@ public class InventorySale {
     @OneToMany(mappedBy = "inventorySale", cascade = CascadeType.ALL, orphanRemoval = true)
     @Schema(description = "구매 요청 목록")
     private List<InventoryPurchase> purchaseList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "inventorySale", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Schema(description = "구매 요청 목록")
+    private List<Images> imageList = new ArrayList<>();
 
     public enum saleStatus {
         available,
