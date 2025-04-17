5a043ed0069cf4181545fc50a8c12275c5485da7
scy <tjcksdud0221@naver.com>
2025-04-17 15:03:55 +0900

    메뉴 도메인, 메뉴 등록 dto 수정

--- src/main/java/com/example/be12fin5verdosewmthisbe/menu_management/menu/model/dto/MenuRegistrationDto.java
+++ src/main/java/com/example/be12fin5verdosewmthisbe/menu_management/menu/model/dto/MenuRegistrationDto.java
@@ -1,6 +1,8 @@
 package com.example.be12fin5verdosewmthisbe.menu_management.menu.model.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,31 +11,19 @@ import java.util.List;
 
 public class MenuRegistrationDto {
 
-    @Schema(description = "메뉴 등록 요청 DTO")
-    @Getter
-    @Setter
-    public static class RequestDto {
-        @Schema(description = "메뉴 이름", example = "비빔밥")
+    @Data
+    @Builder
+    public static class MenuCreateRequestDto {
         private String name;
-
-        @Schema(description = "메뉴 가격", example = "9000")
-        private int price;
-
-        @Schema(description = "카테고리 ID", example = "2")
         private Long categoryId;
+        private int price;
+        private List<IngredientDto> ingredients;
 
-        @Schema(description = "레시피 정보 목록")
-        private List<RecipeInfoDto> recipes;
-    }
-
-    @Schema(description = "레시피 정보 DTO")
-    @Getter
-    @Setter
-    public static class RecipeInfoDto {
-        @Schema(description = "재고 ID", example = "ingredient-1")
-        private String inventoryId;
-
-        @Schema(description = "사용량", example = "100.0")
-        private BigDecimal quantity;
+        @Data
+        @Builder
+        public static class IngredientDto {
+            private Long storeInventoryId;
+            private BigDecimal quantity;
+        }
     }
 }
