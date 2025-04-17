8353bdd75a9034655a1d3c2be2881ce4bf96de8f
scy <tjcksdud0221@naver.com>
2025-04-17 15:08:39 +0900

    재고관리 에러코드 추가

--- src/main/java/com/example/be12fin5verdosewmthisbe/common/ErrorCode.java
+++ src/main/java/com/example/be12fin5verdosewmthisbe/common/ErrorCode.java
@@ -57,7 +57,8 @@ public enum ErrorCode {
     INVENTORY_DELETE_FAIL(7002,"해당 재고 정보를 삭제를 실패했습니다."),
     INVENTORY_UPDATE_FAIL(7003,"해당 재고 수정에 실패했습니다."),
     INVENTORY_REGISTER_FAIL(7004,"해당 재고 정보를 등록하는데 실패했습니다."),
-    INVENTORY_DUPLICATE_NAME(7005, "이미 존재하는 재고 이름입니다.");
+    INVENTORY_DUPLICATE_NAME(7005, "이미 존재하는 재고 이름입니다."),
+    STORE_INVENTORY_NOT_FOUND(7006, "재고 표준 정보가 존재하지않습니다.");
 
 
     private final int status;
