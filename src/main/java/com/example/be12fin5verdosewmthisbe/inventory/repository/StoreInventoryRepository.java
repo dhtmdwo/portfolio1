59536a31dde8589d3ac623b378b99801c2c734ff
taehoon0518 <cjsxogns0518@gmail.com>
2025-04-23 17:34:06 +0900

    [fix] POS 주문 시 재고 자동화 관리 및 Error코드

--- src/main/java/com/example/be12fin5verdosewmthisbe/inventory/repository/StoreInventoryRepository.java
+++ src/main/java/com/example/be12fin5verdosewmthisbe/inventory/repository/StoreInventoryRepository.java
@@ -2,6 +2,7 @@ package com.example.be12fin5verdosewmthisbe.inventory.repository;
 
 import com.example.be12fin5verdosewmthisbe.inventory.model.Inventory;
 import com.example.be12fin5verdosewmthisbe.inventory.model.StoreInventory;
+import com.example.be12fin5verdosewmthisbe.menu_management.menu.model.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -29,5 +30,8 @@ public interface StoreInventoryRepository extends JpaRepository<StoreInventory, 
     List<StoreInventory> findAllStoreInventoryByStore(@Param("storeId") Long storeId);
 
 
+    List<StoreInventory> findByStore_IdAndRecipeList(Long storeId, Recipe recipeList);
+
+    StoreInventory findStoreInventoryByStore_Id(Long id);
 }
 
