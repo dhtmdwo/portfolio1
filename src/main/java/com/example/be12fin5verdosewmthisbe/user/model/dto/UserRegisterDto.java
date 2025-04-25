d997fc3954ac71b1c9cb096ae2cb8bc8a0849c3a
chan0o0seo <tjcksdud0221@naver.com>
2025-04-26 00:06:11 +0900

    UserRegisterDto에 입력값 검증 추가

--- src/main/java/com/example/be12fin5verdosewmthisbe/user/model/dto/UserRegisterDto.java
+++ src/main/java/com/example/be12fin5verdosewmthisbe/user/model/dto/UserRegisterDto.java
@@ -10,12 +10,35 @@ public class UserRegisterDto {
     @Getter
     public static class SignupRequest {
 
+        @NotBlank(message = "이름은 필수입니다.")
         private String name;
+
+        @NotBlank(message = "이메일은 필수입니다.")
+        @Email(message = "올바른 이메일 형식이어야 합니다.")
         private String email;
+
+        @NotBlank(message = "비밀번호는 필수입니다.")
+        @Pattern(
+                regexp = "^(?=.*[A-Z])(?=.*[!@#$%^&*()\\-_=+{};:,<.>]).{8,}$",
+                message = "비밀번호는 8자리 이상이며, 대문자와 특수문자를 포함해야 합니다."
+        )
         private String password;
+
+        @NotBlank(message = "사업자 등록번호는 필수입니다.")
+        @Pattern(
+                regexp = "^\\d{3}-\\d{2}-\\d{5}$",
+                message = "사업자 등록번호는 000-00-00000 형식이어야 합니다."
+        )
         private String businessNumber;
+
+        @NotBlank(message = "전화번호는 필수입니다.")
+        @Pattern(regexp = "^010-\\d{4}-\\d{4}$", message = "전화번호는 010-xxxx-xxxx 형식이어야 합니다.")
         private String phoneNumber;
+
+        @NotBlank(message = "주민등록번호는 필수입니다.")
+        @Pattern(regexp = "^\\d{6}-\\d{1}$", message = "주민등록번호는 yymmdd-1 형식이어야 합니다.")
         private String ssn;
+
         public User toEntity(String encodedPassword) {
             User user = User.builder()
                     .name(name)
