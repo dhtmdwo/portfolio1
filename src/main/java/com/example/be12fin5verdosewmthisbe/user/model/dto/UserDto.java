5a695c5b9102d52b324004ac0cf3671ed8df6dff
chan0o0seo <tjcksdud0221@naver.com>
2025-04-25 23:58:54 +0900

    UserDto에 입력값 검증 수정

--- src/main/java/com/example/be12fin5verdosewmthisbe/user/model/dto/UserDto.java
+++ src/main/java/com/example/be12fin5verdosewmthisbe/user/model/dto/UserDto.java
@@ -9,7 +9,7 @@ public class UserDto {
     public static class LoginRequest {
         @NotBlank(message = "이메일은 필수입니다.")
         private String email;
-        @NotBlank(message = "이메일은 필수입니다.")
+        @NotBlank(message = "비밀번호는 필수입니다.")
         private String password;
     }
 }
