5ffbb0abd975b8cc2f39be10e9e2192663d1ec77
scy <tjcksdud0221@naver.com>
2025-04-30 14:00:34 +0900

    aop 다시 설정

--- src/main/java/com/example/be12fin5verdosewmthisbe/dbtransaction/ReadOnlyTransactional.java
+++
@@ -1,9 +0,0 @@
-package com.example.be12fin5verdosewmthisbe.dbtransaction;
-
-import java.lang.annotation.*;
-
-@Target({ElementType.METHOD, ElementType.TYPE})
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface ReadOnlyTransactional {
-}
